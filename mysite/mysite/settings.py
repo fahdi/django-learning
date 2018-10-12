d071fb8cceb6f2a1882ede2d4ba221f4decb2563
Fahad Murtaza <fahdim@gmail.com>
2018-10-12 18:13:50 +0500

    Adding new requirements

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&gpj+1b85b1_1!$j4(kgep0-dc211@83k-*u7+d+(knu=r%u25'
+SECRET_KEY = 'r2am33i2g90oqrqr1^(fx^$9egoydvd+&kb2m*j)gm&puvcypw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
